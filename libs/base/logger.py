9f4f8fb24d104fa55fcc6ae464d0a04fd1c8255b
Guciii <nowak.tomasz1337@gmail.com>
2020-05-20 16:13:35 +0200

    [ADD/FIX] Poprawne tworzenie pliku debug.log

--- libs/base/logger.py
+++ libs/base/logger.py
@@ -1,21 +1,33 @@
+from sys import platform
 import logging
+import os
 
 
 def configure_logging(mode):
+
+    if platform == "win32":
+        logpath = "TechDraw3D/logs/debug.log"
+    
+    if platform == "linux":
+        logpath = "logs/debug.log"
+
     try:
         logging.basicConfig(level=mode,
             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
             datefmt='%m-%d %H:%M',
-            filename='logs/debug.log',
+            filename=logpath,
             filemode='a')
 
     except FileNotFoundError as e:
-        open("logs/debug.log", 'a').close()
+        if not os.path.isdir(logpath[:-10]):
+            os.mkdir(logpath[:-10])
+
+        open(logpath, 'a').close()
 
         logging.basicConfig(level=mode,
             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
             datefmt='%m-%d %H:%M',
-            filename='logs/debug.log',
+            filename=logpath,
             filemode='a')
 
     # define a Handler which writes INFO messages or higher to the sys.stderr
