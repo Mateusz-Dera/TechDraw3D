5ab70d864464a66436271cde6e050c8b679751e7
Jakub Schwarz <jschwarz@litex.pl>
2020-05-21 17:53:57 +0200

    [EDIT] Drobne porządki

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 # My modules
 from libs.extruder.svg import SVG
 from libs.extruder.dwginput import DWGInput
-from libs.base import logger, args
+from libs.base import logger, args, argparser
 from libs.base.argparser import *
 
 _logger = logging.getLogger(__name__)
@@ -15,21 +15,20 @@ if __name__ == '__main__':
     
     _logger.info("Start program")
 
-    # svgObj = SVG()
-    # res = svgObj.create_svg_object("data/iw.svg", kwargs={'logger': _logger})
 
     # Uruchomienie parsera argumentów.
-    parser = make_parser()
+    parser = argparser.make_parser()
     args = parser.parse_args()
-    print(args)
+    
+    # print(args)
 
     # Uruchomienie funkcji konwertującej dwg2svg.
     dwg = DWGInput()
     dwg.dwg2svg_converter(args.file.name)
 
-    #print(dwg.DWG)
+    # Obsługa SVG
 
-    #dwg.returnSVG()
-
-    # a, b, c = svgObj.parse_svg_dict(res)
+    svgObj = SVG()
+    res = svgObj.create_svg_object("data/iw.svg", kwargs={'logger': _logger})
+    a, b, c = svgObj.parse_svg_dict(res)
 
