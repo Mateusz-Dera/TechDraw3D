a8c83fdd2cc3425ee4e20bdaaea5feb65ada285f
Guciii <nowak.tomasz1337@gmail.com>
2020-12-04 21:29:57 +0100

    [FIX] Literówka

--- linux_install.sh
+++ linux_install.sh
@@ -16,5 +16,5 @@ else
     exit 1
 fi
 
-sudo python3 -m pip install pyvista simple-term-menu svgpathtools dxf2svg ezdxt
+sudo python3 -m pip install pyvista simple-term-menu svgpathtools dxf2svg ezdxf
 sudo snap install docker
