db8c6d9cecd9f3433d30c3eb566f4d9a639c2337
Mateusz-Dera <otahopl@gmail.com>
2020-12-04 20:12:22 +0100

    python

--- linux_install.sh
+++ linux_install.sh
@@ -6,11 +6,11 @@ echo "Fedora 32"
 
 if hash rpm 2>/dev/null; then
     echo "Installation type Fedora"
-    sudo dnf install snapd blender
+    sudo dnf install snapd blender python3 python3-pip
     sudo ln -s /var/lib/snapd/snap /snap
 elif hash apt 2>/dev/null; then
     echo "Installation type Ubuntu"
-    sudo apt-get -y install snapd blender
+    sudo apt-get -y install snapd blender python3 python3-pip
 else
     echo "Not supported distro"
     exit 1
