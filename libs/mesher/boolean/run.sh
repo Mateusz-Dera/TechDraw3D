a1f89c5c8b66d64bf6857544e416d841fe7cea30
Mateusz-Dera <otahopl@gmail.com>
2020-12-04 22:58:20 +0100

    Copiright

--- libs/mesher/boolean/run.sh
+++ libs/mesher/boolean/run.sh
@@ -1,4 +1,24 @@
-# Mateusz Dera
+# TechDraw3D
+# Copyright © 2020 Tomasz Nowak, Mateusz Dera, Jakub Schwarz
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 DIRECTORY="$(cd "$(dirname "$0")" && pwd -P)/$(basename "$1")"
 
 #sudo docker run -it --rm -v $DIRECTORY:/root qnzhou/pymesh bash
