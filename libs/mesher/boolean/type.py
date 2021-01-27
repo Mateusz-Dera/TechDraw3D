819fc6aa932251dc73d733a58ff357f393d554d7
Guciii <nowak.tomasz1337@gmail.com>
2021-01-27 23:20:08 +0100

    [FIX] Naprawa ścieżek boolean

--- libs/mesher/boolean/type.py
+++ libs/mesher/boolean/type.py
@@ -27,7 +27,7 @@ import sys
 path = str(sys.argv[3])[:-7] + "export/"
 #export_path = str(sys.argv[3])[:-7] + "export/"
 export_path = str(sys.argv[3])[:-7] + str(sys.argv[5])
-mesh_path = str(path)
+mesh_path = str(path) + 'mesh.obj'
 
 # Czyszczenie sceny ze wszystkich domyślnie ładowanych obiektów
 bpy.ops.object.select_all(action='SELECT')
@@ -42,15 +42,15 @@ for obj in bpy.context.selected_objects:
 # STL
 typ = '-stl'
 if str(sys.argv[4]) == typ:
-    bpy.ops.export_mesh.stl(filepath=str(export_path) + 'exported-mesh.stl',use_selection=True)
+    bpy.ops.export_mesh.stl(filepath=str(export_path), use_selection=True)
 # FBX
 typ = '-fbx'
 if str(sys.argv[4]) == typ:
-    bpy.ops.export_scene.fbx(filepath=str(export_path) + 'exported-mesh.fbx', use_selection = True)
+    bpy.ops.export_scene.fbx(filepath=str(export_path), use_selection = True)
 
 # OBJ
 typ = '-obj'
 if str(sys.argv[4]) == typ:
-    bpy.ops.export_scene.obj(filepath=str(export_path) + 'exported-mesh.obj', check_existing=False, axis_forward='-Z', axis_up='Y', filter_glob="*.obj;*.mtl", use_selection=True, use_animation=False, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=True, use_uvs=True, use_materials=True, use_triangles=False, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=False, global_scale=1, path_mode='AUTO')
+    bpy.ops.export_scene.obj(filepath=str(export_path), check_existing=False, axis_forward='-Z', axis_up='Y', filter_glob="*.obj;*.mtl", use_selection=True, use_animation=False, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=True, use_uvs=True, use_materials=True, use_triangles=False, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=False, global_scale=1, path_mode='AUTO')
 
 os.remove(mesh_path)
