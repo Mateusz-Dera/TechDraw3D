32b884940db37fccb2d2a1ced0a34c94acb7da42
Jakub Schwarz <jschwarz@litex.pl>
2021-01-29 23:59:22 +0100

    [FIX] Próba obejścia błędu

--- libs/extruder/dwginput.py
+++ libs/extruder/dwginput.py
@@ -96,4 +96,6 @@ class DWGInput():
                 os.mkdir(os.path.dirname(output_file))
 
             subprocess.call([dwg2dxf_linux + ' ' + parameter1 + ' ' + parameter2 + ' ' + output_file + ' ' + dwgfilepath], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+            wait(10)
             return output_file
